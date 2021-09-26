86707c468d331d1935fd77316145d33df2de9919
us <crqiurl@163.com>
2021-09-27 00:25:15 +0800

    这是一个项目根

---
+++ demo-01/src/main/java/com/example/web/WebContorller.java
@@ -0,0 +1,14 @@
+package com.example.web;
+
+/**
+ * @author 月亮与六便士
+ * @version v1.0
+ * @Package com.example.web
+ * @date 2021/9/27 0:20
+ * @copyright 千羊科技
+ */
+public class WebContorller {
+    public static void main(String[] args) {
+        System.out.println("helloword");
+    }
+}
